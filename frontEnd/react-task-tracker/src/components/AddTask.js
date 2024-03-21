bc62aae6edc67529733f587077f7af487f6994c4
Sahadat Islam Evan <126870464+combo666@users.noreply.github.com>
2024-03-21 19:07:29 +0600

    small change

--- frontEnd/react-task-tracker/src/components/AddTask.js
+++ frontEnd/react-task-tracker/src/components/AddTask.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 const AddTask = () => {
-    const [text, setTex] = useState('')
+    const [text, setText] = useState('')
     const [day,setDay] = useState('')
     const [details, setDetails] = useState('')
     const [reminder, setReminder] = useState(false)
@@ -11,15 +11,15 @@ const AddTask = () => {
     <form className='add-form' >
         <div className="form-control" >
             <label>Task</label>
-            <input type="text" placeholder="Add Task" />
+            <input type="text" placeholder="Add Task" value= {text} onChange={(e) => setText(e.target.value)} />
         </div>
         <div className="form-control" >
             <label>Date and Time </label>
-            <input type="text" placeholder="Add Task" />
+            <input type="text" placeholder="Add Task" value= {day} onChange={(e) => setDay(e.target.value)}/>
         </div>
         <div className="form-control" >
             <label>Details</label>
-            <input type="text" placeholder="Add Date and Time" />
+            <input type="text" placeholder="Add Date and Time" value= {reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
         </div>
         <div className="form-control form-control-check" >
             <label>Remainder</label>
